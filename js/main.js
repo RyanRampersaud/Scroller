b0ebdd91c21957b6efd63b345cd27fd4d7c8d6f5
Graphic <graphic@SOD-SJH-516-10M.local>
2015-10-23 14:48:04 -0400

    scroll bar added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(window).resize(function () {
+// console.log( $(this).height() + ", " + $(this).width() );
+// }); 
+
+$(window).on('scroll',function(e){
+
+	var px = $(window).scrollTop();
+	console.log(px);
+
+	$('.hills')
+});
